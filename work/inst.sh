ada2ffef582da01fe93a300a67cb85b893eba684
Peter.DeRijk <Peter.DeRijk@ua.ac.be>
2009-04-30 16:30:48 +0000

    work/inst fix: automatic version, quickinst ok

--- work/inst.sh
+++ work/inst.sh
@@ -1,9 +1,14 @@
+#!/bin/sh
+
+# $Format: "export version=$ProjectMajorVersion$.$ProjectMinorVersion$.$ProjectPatchLevel$"$
+export version=1.0.2
+
 # full compile and install linux
 cd /home/peter/dev/ClassyTcl/Linux-i686
 make distclean
 ../configure --prefix=/home/peter/tcl/dirtcl
 make
-rm -rf /home/peter/build/tca/Linux-i686/exts/Class1.0.2/
+rm -rf /home/peter/build/tca/Linux-i686/exts/Class$version/
 /home/peter/dev/ClassyTcl/build/install.tcl /home/peter/build/tca/Linux-i686/exts
 
 # full cross-compile and install windows
@@ -11,7 +16,7 @@ cd /home/peter/dev/ClassyTcl/windows-intel
 make distclean
 cross-bconfigure.sh --prefix=/home/peter/tcl/win-dirtcl
 cross-make.sh
-rm -rf /home/peter/build/tca/Windows-intel/exts/Class1.0.2/
+rm -rf /home/peter/build/tca/Windows-intel/exts/Class$version/
 wine /home/peter/build/tca/Windows-intel/tclsh84.exe /home/peter/dev/ClassyTcl/build/install.tcl /home/peter/build/tca/Windows-intel/exts
 
 /home/peter/dev/ClassyTcl/build/version.tcl
--- work/quickinst.sh
+++ work/quickinst.sh
@@ -1,3 +1,22 @@
+#!/bin/sh
+
+# $Format: "export version=$ProjectMajorVersion$.$ProjectMinorVersion$.$ProjectPatchLevel$"$
+export version=1.0.2
+
 # quick install
-rm -rf /home/peter/build/tca/Linux-i686/exts/Class1.0.2/
+#!/bin/sh
+
+# $Format: "export version=$ProjectMajorVersion$.$ProjectMinorVersion$.$ProjectPatchLevel$"$
+export version=1.0.2
+
+# quick install linux
+cd /home/peter/dev/ClassyTcl/Linux-i686
+rm -rf /home/peter/build/tca/Linux-i686/exts/Class$version/
 /home/peter/dev/ClassyTcl/build/install.tcl /home/peter/build/tca/Linux-i686/exts
+
+# quick install windows
+cd /home/peter/dev/ClassyTcl/windows-intel
+rm -rf /home/peter/build/tca/Windows-intel/exts/Class$version/
+wine /home/peter/build/tca/Windows-intel/tclsh84.exe /home/peter/dev/ClassyTcl/build/install.tcl /home/peter/build/tca/Windows-intel/exts
+
+/home/peter/dev/ClassyTcl/build/version.tcl
